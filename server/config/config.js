9d35110f9846648e54653b7c96074975acf3e342
= <=>
2020-02-02 21:29:33 -0500

    Google sign in API

--- server/config/config.js
+++ server/config/config.js
@@ -26,3 +26,7 @@ process.env.CADUCIDAD_TOKEN= 60 * 60 * 24 * 30;
 // ====SEED autenticacion =====
 
 process.env.SEED = process.env.SEED || 'SECRET'
+
+
+// ====Google CLIENT_ID=====
+process.env.CLIENT_ID = process.env.CLIENT_ID || '282954723664-2av6k0n6h2318obmkbkik0p4c24ngqhp.apps.googleusercontent.com';
